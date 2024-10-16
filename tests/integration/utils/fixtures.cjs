41bb5ad78ce143fec257b706577decb229d97882
tryb3l <56679619+tryb3l@users.noreply.github.com>
2024-10-16 23:55:41 +0200

    Add utility function to create test user

---
+++ tests/integration/utils/fixtures.cjs
@@ -0,0 +1,19 @@
+const {
+  randomEmail,
+  randomPassword,
+  randomUsername,
+} = require('./data-creator.cjs');
+
+function createTestUser() {
+  return {
+    user: {
+      email: randomEmail(10, 5),
+      password: randomPassword(10),
+      username: randomUsername(10),
+    },
+  };
+}
+
+module.exports = {
+  createTestUser,
+};
